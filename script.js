bb53e20e17754da10372562a9fe9afcc66bb15c8
KkOoSsTtAa <nemanja.kostadinovic9@gmail.com>
2022-07-02 15:13:28 +0200

    version 1.

--- script.js
+++ script.js
@@ -1,38 +1,80 @@
-const burgeri = document.getElementById('burgeri');
-const krilca = document.getElementById('krilca');
-const piletina = document.getElementById('piletina');
-const slatkisi = document.getElementById('slatkisi');
+const burgeri = document.getElementById("burgeri");
+const krilca = document.getElementById("krilca");
+const piletina = document.getElementById("piletina");
+const slatkisi = document.getElementById("slatkisi");
+const burgerMenu = document.querySelector(".burger-menu");
+const header = document.getElementById("header");
 
-const sviBurgeri = document.querySelectorAll('.visakB')
-const svaKrilca = document.querySelectorAll('.visakK')
-const svaPiletina = document.querySelectorAll('.visakP')
-const sviSlatkisi = document.querySelectorAll('.visakS')
+const modal = document.getElementById("myModal");
+const btn = document.getElementById("poruci");
+const span = document.getElementsByClassName("close")[0];
 
+const sviBurgeri = document.querySelectorAll(".visakB");
+const svaKrilca = document.querySelectorAll(".visakK");
+const svaPiletina = document.querySelectorAll(".visakP");
+const sviSlatkisi = document.querySelectorAll(".visakS");
+const nav = document.querySelector(".nav");
 
-burgeri.addEventListener('click', (e) => {
-    e.preventDefault();
-    sviBurgeri.forEach(element => {
-        element.classList.toggle('hidden');
-    });
+burgeri.addEventListener("click", (e) => {
+  e.preventDefault();
+  sviBurgeri.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
+});
+
+krilca.addEventListener("click", (e) => {
+  e.preventDefault();
+  svaKrilca.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
+});
+
+piletina.addEventListener("click", (e) => {
+  e.preventDefault();
+  svaPiletina.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
+});
+
+slatkisi.addEventListener("click", (e) => {
+  e.preventDefault();
+  sviSlatkisi.forEach((element) => {
+    element.classList.toggle("hidden");
+  });
+});
+
+burgerMenu.addEventListener("click", (e) => {
+  e.preventDefault();
+  nav.classList.toggle("nav-mobile");
+});
+
+document.addEventListener("click", (e) => {
+  var isClickInsideElement = burgerMenu.contains(e.target);
+  if (!isClickInsideElement) {
+    nav.classList.remove("nav-mobile");
+  }
+});
+
+document.addEventListener("scroll", (e) => {
+  let scroll = window.scrollY;
+  if (scroll > 800) {
+    header.classList.add('sticky');
+  } else {
+    header.classList.remove('sticky');
+  }
+});
+
+btn.addEventListener('click', (e) => {
+  modal.style.display = "block";
 })
 
-krilca.addEventListener('click', (e) => {
-    e.preventDefault();
-    svaKrilca.forEach(element => {
-        element.classList.toggle('hidden');
-    });
+span.addEventListener('click', (e) => {
+  modal.style.display = "none";
+
 })
 
-piletina.addEventListener('click', (e) => {
-    e.preventDefault();
-    svaPiletina.forEach(element => {
-        element.classList.toggle('hidden');
-    });
-})
-
-slatkisi.addEventListener('click', (e) => {
-    e.preventDefault();
-    sviSlatkisi.forEach(element => {
-        element.classList.toggle('hidden');
-    });
+window.addEventListener('click', (e) => {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 })
